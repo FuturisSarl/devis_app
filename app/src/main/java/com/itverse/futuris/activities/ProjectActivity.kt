94734fa323a733a39a9475a862ddd9c3aecfaa55
Shalum11059 <moisedete@gmail.com>
2021-10-23 18:58:27 -0400

    Fixed KaptDebugException error and changed ProjectActivity to use the Db to load data

--- app/src/main/java/com/itverse/futuris/activities/ProjectActivity.kt
+++ app/src/main/java/com/itverse/futuris/activities/ProjectActivity.kt
@@ -3,12 +3,15 @@ package com.itverse.futuris.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.itverse.futuris.*
 import com.itverse.futuris.adapters.ProjectRecyclerAdapter
+import com.itverse.futuris.mViewModels.ProjectViewModel
+import com.itverse.futuris.mViewModels.ProjectViewModelFactory
 import com.itverse.futuris.utils.createProjectFromTemplate
 import kotlinx.android.synthetic.main.content_activity_project.*
 
@@ -20,17 +23,18 @@ class ProjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project)
-        //setSupportActionBar(findViewById(R.id.toolbar))
+//        setSupportActionBar(findViewById(R.id.toolbar))
 
         project_list.layoutManager = GridLayoutManager(this,2)
 
-        loadProjects(this)
+        val adapter = ProjectRecyclerAdapter(this)
 
-        project_list.adapter =
-            ProjectRecyclerAdapter(
-                this,
-                DataManager.projects
-            )
+        projectViewModel.allProjects.observe(this) { projects ->
+            // Update the cached copy of the project in the adapter.
+            projects.let { adapter.submitList(it) }
+        }
+
+        project_list.adapter = adapter
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             val intent = Intent(this, CreateNewProject::class.java)
